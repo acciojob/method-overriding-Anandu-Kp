402b89964e5893ee5550540c250a713ed29df733
Anandu-Kp <anandukp06@gmail.com>
2023-01-15 10:51:18 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B b=new B();
+        b.meth();
+        b.meth();
+    }
+
+}
+class A
+{
+    public static String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
+class B extends A
+{
+    public static String meth()
+    {
+        return "Method is overridden in Extendend class B";
+    }
 }
